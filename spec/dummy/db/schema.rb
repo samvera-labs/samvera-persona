58954dc24cc35a265e1a497b097c852a8886ba9e
Lea Ann Bradford <leaann@notch8.com>
2019-05-13 12:18:43 -0700

    user table migration

--- spec/dummy/db/schema.rb
+++ spec/dummy/db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2019_05_13_181646) do
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
+    t.string "password_confirmation"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
