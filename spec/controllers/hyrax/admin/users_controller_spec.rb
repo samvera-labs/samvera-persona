bd80e01cf405bfd06627eed4f59227cf183ac859
Lea Ann Bradford <leaann@notch8.com>
2019-05-13 12:18:13 -0700

    move tests to correct file

--- spec/controllers/hyrax/admin/users_controller_spec.rb
+++ spec/controllers/hyrax/admin/users_controller_spec.rb
@@ -1,3 +1,5 @@
+require 'rails_helper'
+
 RSpec.describe Hyrax::Admin::UsersController, type: :controller do
   before do
     expect(controller).to receive(:authorize!).with(:read, :admin_dashboard).and_return(true)
@@ -12,6 +14,38 @@ RSpec.describe Hyrax::Admin::UsersController, type: :controller do
       get :index
       expect(response).to be_successful
       expect(assigns[:presenter]).to be_kind_of Hyrax::Admin::UsersPresenter
+    end
+  end
+
+  context 'as an anonymous user' do
+    let(:user) { FactoryBot.create(:user) }
+
+    describe 'DELETE #destroy' do
+      subject { User.find_by(id: user.id) }
+
+      before { delete :destroy, params: { id: user.id } }
+
+      it "doesn't delete the user and redirects to login" do
+        expect(subject).not_to be_nil
+        expect(response).to redirect_to root_path
+      end
+    end
+  end
+
+  context 'as an admin user' do
+    let(:user) { FactoryBot.create(:user) }
+
+    before { sign_in create(:admin) }
+
+    describe 'DELETE #destroy' do
+      subject { User.find_by(id: user.id) }
+
+      before { delete :destroy, params: { id: user.to_param } }
+
+      it "deletes the user and displays success message" do
+        expect(subject).to be_nil
+        expect(flash[:notice]).to eq "User \"#{user.email}\" has been successfully deleted."
+      end
     end
   end
 end
