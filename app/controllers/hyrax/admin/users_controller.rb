cdefe3679c1ae56699f49ceaa86ebc323f460bc2
Lea Ann Bradford <leaann@notch8.com>
2019-05-14 15:11:55 -0700

    wip - start adding edit function

--- app/controllers/hyrax/admin/users_controller.rb
+++ app/controllers/hyrax/admin/users_controller.rb
@@ -2,9 +2,32 @@ module Hyrax
   class Admin::UsersController < ApplicationController
     include Hyrax::Admin::UsersControllerBehavior
 
-    before_action :load_user, only: [:destroy]
+    before_action :load_user, only: [:edit, :update, :destroy]
 
     # NOTE: User creation/invitations handled by devise_invitable
+
+    # GET /admin/users/1/edit
+    def edit
+      #TODO - are we adding breadcrumbs conditionally here or are they added by the local app
+
+      # add_breadcrumb t(:'hyrax.controls.home'), root_path
+      # add_breadcrumb t(:'hyrax.admin.sidebar.users'), proprietor_users_path
+      # add_breadcrumb @user.display_name, edit_proprietor_user_path(@user)
+    end
+
+    # PATCH/PUT admin/users/1
+    # PATCH/PUT admin/users/1.json
+    def update
+      respond_to do |format|
+        if @user.update(user_params)
+          format.html { redirect_to admin_users_path, notice: 'User was successfully updated.' }
+          # format.json { render :show, status: :ok #, location: [:proprietor, @user] }
+        else
+          format.html { render :edit }
+          # format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
+      end
+    end
 
     # Delete a user from the site
     def destroy
--- lib/user_management.rb
+++ lib/user_management.rb
@@ -1,5 +1,13 @@
-require "user_management/engine"
+# requires all dependencies
+Gem.loaded_specs['user_management'].dependencies.each do |d|
+  require d.name unless d.type == :development
+ end
+ 
+ require "user_management/engine"
 
 module UserManagement
+  # class Engine < ::Rails::Engine
+  #   isolate_namespace UserManagement
+  # end
   # Your code goes here...
 end
