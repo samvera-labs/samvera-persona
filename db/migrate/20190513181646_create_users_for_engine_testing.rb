523b2c2f1525a4d7943cc2707639eee85d3fd010
Lea Ann Bradford <leaann@notch8.com>
2019-05-20 12:32:29 -0700

    add user table migration back

---
+++ db/migrate/20190513181646_create_users_for_engine_testing.rb
@@ -0,0 +1,13 @@
+class CreateUsersForEngineTesting < ActiveRecord::Migration[5.2]
+  def change
+    unless table_exists? :users
+      create_table :users do |t|
+        t.string :email
+        t.string :password
+        t.string :password_confirmation
+
+        t.timestamps
+      end
+    end
+  end
+end
