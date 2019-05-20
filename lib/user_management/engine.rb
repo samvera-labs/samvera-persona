13281c748a46b955067f2f4bf00a4ee8af11c2d4
Lea Ann Bradford <leaann@notch8.com>
2019-05-20 10:46:33 -0700

    add migration for soft delete - paranoia gem

--- lib/user_management/engine.rb
+++ lib/user_management/engine.rb
@@ -3,6 +3,16 @@ require 'paranoia'
 
 module UserManagement
   class Engine < ::Rails::Engine
+    isolate_namespace UserManagement
+
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
+      end
+    end
+
     config.generators do |g|
       g.test_framework :rspec, :fixture => false
       g.fixture_replacement :factory_bot, :dir => 'spec/factories'
