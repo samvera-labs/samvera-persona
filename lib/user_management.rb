c6be5badca77ef7844997e6d66bf2eaf487b55b0
Lea Ann Bradford <leaann@notch8.com>
2019-05-20 17:24:45 -0700

    remove extra code

--- lib/user_management.rb
+++ lib/user_management.rb
@@ -6,8 +6,5 @@ Gem.loaded_specs['user_management'].dependencies.each do |d|
  require "user_management/engine"
 
 module UserManagement
-  # class Engine < ::Rails::Engine
-  #   isolate_namespace UserManagement
-  # end
-  # Your code goes here...
+  
 end
