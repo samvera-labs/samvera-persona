8ec5cc8b4d6fbcfa03eca3f1b6bd3dbea4c2efc9
Lea Ann Bradford <leaann@notch8.com>
2019-06-14 17:32:35 -0700

    added user model to dummy app

---
+++ spec/dummy/app/models/user.rb
@@ -0,0 +1,27 @@
+class User < ApplicationRecord
+  attr_accessor :roles
+  # devise :database_authenticatable, :confirmable, :invitable
+
+  def roles
+    @roles ||= []
+  end
+
+  def add_role(role)
+    roles << role unless roles.include?(role)
+  end
+
+  def remove_role(role)
+    roles.delete(role)
+  end
+
+  def groups=(value)
+    roles = value
+  end
+
+  def groups
+    roles
+  end
+
+
+end
+
